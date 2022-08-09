b21716657acdf3917422d98173155035674eb8bc
jakegourley <jake@gourley2.co.uk>
2022-08-09 14:22:10 +0100

    edited js

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ function buttonClicked(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
             //used for debugging
-            //console.log(response.farewell);
+            //console.log(response.farewell[0]);
         });
     });
 }
